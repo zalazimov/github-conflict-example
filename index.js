e68caf9814ed2e64d68db2de1bba18d6d6becbba
zazimov <zalmanazimov@pursuit.org>
2023-05-19 13:04:52 -0400

    refactored addPersonAsFriend code

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function createPerson(name, age) {
   };
 }
 
-function addPersonAsFriend(person, other) {
-  person.friends.push(other);
+function addPersonAsFriend(person, friend) {
+  person.friends.push(friend);
   return person;
 }
 
